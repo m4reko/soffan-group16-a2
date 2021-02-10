4434d92fb4654077f32dbded2dccaa7ba1daf6d3
Justin Arieltan <agriad1@yahoo.com>
2021-02-10 16:37:07 +0100

    Add documentation (#10)
    Co-authored-by: tjr16 <jerryaccount@126.com>

--- app/src/main/java/ciserver/Repository.java
+++ app/src/main/java/ciserver/Repository.java
@@ -8,6 +8,10 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+/**
+ * Handles the repository functions such as to clone, build, and 
+ * report back to Github about the status of it.
+ */
 public class Repository {
 
     public enum CommitStatus {
@@ -17,11 +21,20 @@ public class Repository {
     private CommitStatus commitStatus;
     private Payload payload;
 
+    /**
+     * Constructs a repository object with a payload.
+     * Clones the corresponding remote repository.
+     *
+     * @param payload webhook payload
+     */
     public Repository(Payload payload) throws IOException {
         this.payload = payload;
         cloneRemote();
     }
 
+    /**
+     * Clones the remote repository that is sent in the payload.
+     */
     private void cloneRemote() throws IOException {
         String cloneUrl = this.payload.getCloneUrl();
         File file = new File("src/cloneRemote");
@@ -36,9 +49,17 @@ public class Repository {
         }
     }
 
+    /**
+     * Runs the Gradle build and checks to see if the build is successful or not.
+     * It would set the commitStatus depending on the outcome of the build command.
+     */
     public void build() {
     }
 
+    /**
+     * Sends the commit status after building.
+     * Uses REST API for Github or emails project members.
+     */
     public void reportCommitStatus() {
         throw new NotImplementedException();
     }
