53c408fd5676cd7dc805d0e4e21be515b6346b7b
Justin Arieltan <agriad1@yahoo.com>
2021-02-12 13:29:09 +0100

    Add clone remote function, resolve #10 (#14)
    * Add clone remote function (#10)
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    * Add repository constructor (#10)
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    * Fix missing exception (#10)
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    * Add documentation (#10)
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    * Add branch selection for cloning (#10)
    
    Added a missing feature that selects a branch before cloning.
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    * Refactor clone remote function (#10)
    
    Refactored the clone remote function so that it is easier to create
    a unit test for it.
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    * Add unit test for clone remote function (#10)
    
    Added a get cloned repository location function to help the unit test.
    Added a simple test resource to better fit the unit test requirement.
    Added a unit test for the clone remote function.
    
    Co-authored-by: tjr16 <jerryaccount@126.com>
    
    Co-authored-by: tjr16 <jerryaccount@126.com>

--- app/src/main/java/ciserver/ContinuousIntegrationServer.java
+++ app/src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -32,6 +32,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         Repository repo = new Repository(payload);
 
+        repo.cloneRemote();
         repo.build();
         repo.reportCommitStatus();
 
--- app/src/main/java/ciserver/Repository.java
+++ app/src/main/java/ciserver/Repository.java
@@ -1,8 +1,19 @@
 package ciserver;
 
+import java.io.IOException;
+import java.io.File;
+
+import java.util.UUID;
+
 import org.apache.commons.lang3.NotImplementedException;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
+/**
+ * Handles the repository functions such as to clone, build, and 
+ * report back to Github about the status of it.
+ */
 public class Repository {
 
     public enum CommitStatus {
@@ -11,21 +22,62 @@ public class Repository {
 
     private CommitStatus commitStatus;
     private Payload payload;
+    private String clonedRepositoryLocation;
 
-    public Repository(Payload payload) {
-        throw new NotImplementedException();
+    /**
+     * Constructs a repository object with a payload.
+     * Clones the corresponding remote repository.
+     *
+     * @param payload webhook payload
+     */
+    public Repository(Payload payload) throws IOException {
+        this.payload = payload;
     }
 
-    private void cloneRemote(){
-        throw new NotImplementedException();
+    /**
+     * Clones the remote repository that is sent in the payload.
+     */
+    public String cloneRemote() throws IOException {
+        String cloneUrl = this.payload.getCloneUrl();
+        String branch = this.payload.getRef();
+        String uniqueID = UUID.randomUUID().toString();
+        this.clonedRepositoryLocation = "src/cloneRemote-" + uniqueID;
+        File file = new File(this.clonedRepositoryLocation);
+
+        try (Git git = Git.cloneRepository()
+            .setURI(cloneUrl)
+            .setDirectory(file)
+            .setBranch(branch)
+            .call()) {
+            return "Success";
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            return "Fail";
+        }
     }
 
+    /**
+     * Runs the Gradle build and checks to see if the build is successful or not.
+     * It would set the commitStatus depending on the outcome of the build command.
+     */
     public void build() {
-        throw new NotImplementedException();
     }
 
+    /**
+     * Sends the commit status after building.
+     * Uses REST API for Github or emails project members.
+     */
     public void reportCommitStatus() {
         throw new NotImplementedException();
+    }
+
+    /**
+     * Gets the path of the cloned repository
+     * 
+     * @return String of the location
+     */
+    public String getClonedRepositoryLocation() {
+        return this.clonedRepositoryLocation;
     }
 
 }
